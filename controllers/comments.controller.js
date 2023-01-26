9907e2e57faa41e1a4909d039c4f1ae30d0fff73
thursdaycurry <thursdaycurry@gmail.com>
2023-01-26 21:21:51 +0900

    [#6][Hugo][Feature] Comment 라우터 추가

---
+++ controllers/comments.controller.js
@@ -0,0 +1,58 @@
+const CommentService = require('../services/comments.service');
+
+// todo - Error Handler
+
+class CommentsController {
+
+  commentService = new CommentService();
+
+  createComment = async (req, res, next) => {
+
+    // 선언 파트
+    const userId = 'temp_userId' 
+    const { postId } = req.params; 
+    const { commentContent } = req.body;
+    
+    // 구현 파트
+    const comment = await this.commentService.createComment(
+      userId,
+      postId,
+      commentContent
+    );
+    return res.status(201).json({ statusCode: "201", "message": '댓글 작성에 성공했습니다.' });
+  }
+
+  updateComment = async (req, res, next) => {
+
+    // 선언 파트
+    const userId = 'temp_userId' // todo - 토큰 작업 완료 시, userId PK 가져오는 것으로 수정할 것
+    const { commentId } = req.params;
+    const { commentContent } = req.body;
+    
+    // 구현 파트
+    // todo 토큰 유저와 댓글 작성자 동일 인물인지 체크할 것
+
+    const updateComment = await this.commentService.updateComment(
+      commentId,
+      commentContent
+    );
+    return res.status(204).json({ statusCode: "204", "message": '댓글 수정에 성공했습니다.' });
+  }
+
+  deleteComment = async (req, res, next) => {
+
+    // 선언 파트
+    const userId = 'temp_userId' // todo - 토큰 작업 완료 시, userId PK 가져오는 것으로 수정할 것
+    const { commentId } = req.params;
+
+    // 구현 파트
+    // todo 토큰 유저와 댓글 작성자 동일 인물인지 체크할 것
+    const deleteComment = await this.commentService.deleteComment(
+      commentId
+    );
+    return res.status(204).json({ statusCode: "204", "message": '댓글 삭제에 성공했습니다.' });
+  }
+
+}
+
+module.exports = CommentsController;
