9b633ea5dbf050b53e8db703322a41490a105f82
ljunbin <ljunbin950@gmail.com>
2023-01-28 20:29:33 +0900

    [Leo][feature] post crud

---
+++ repositories/posts.repository.js
@@ -0,0 +1,68 @@
+const { Users, Posts, Comments, Likes } = require("../models");
+const user = require("../models/users");
+
+class PostRepository {
+  // 게시글 전체 조회
+  findAllPost = async () => {
+    const allPost = await Posts.findAll({
+      include: [{ model: Users }, { model: Likes }],
+    });
+
+    return allPost;
+  };
+
+  // 게시글 상세 조회
+  findOnePost = async (postId, userId) => {
+    const postsOne = await Posts.findOne({
+      where: { postId },
+      include: [
+        {
+          model: Users,
+        },
+        {
+          model: Likes,
+        },
+        {
+          model: Comments,
+          include: [
+            { model: Users },
+            { model: Likes, where: { userId: userId }, required: false },
+          ],
+        },
+      ],
+    });
+    console.log(postsOne);
+    return postsOne;
+  };
+
+  // 게시글 수정
+  updatePost = async ({postId, postContent, image}) => {
+    await Posts.update({postContent:postContent, image:image}, {where:{postId:postId}})
+    return ;
+  };
+
+  //게시글 삭제
+  deletePosts = async (postId) => {
+    const deletePost = Posts.destroy({
+      where: { postId },
+    });
+
+    return deletePost;
+  };
+
+  //게시글 업로드
+  createPost = async (imageUrls, userId, postContent) => {
+    const createPost = await Posts.create({
+      image: imageUrls,
+      userId: userId,
+      postContent: postContent,
+    });
+
+    return createPost;
+  };
+
+  //게시글 좋아요
+  
+}
+
+module.exports = PostRepository;
